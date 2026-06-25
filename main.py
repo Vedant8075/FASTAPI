19b288fda927e7c33c0fbd6d8f6656a802f4a402
VEDANT <vedantg663@gmail.com>
2026-06-25 15:06:42 +0530

    gettign request

---
+++ main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI,HTTPException,Query,Path
+from services.products import get_all_products
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "welcome to fastapi"}
+
+@app.get("/products")
+def list_products(
+
+    name: str=Query(
+        default=None
+        ,max_length=100,
+        min_length=1),
+        sort_by: str = Query(default=None),
+        order:str=Query(default="asc")
+        ):
+    
+    products=get_all_products()
+    if name:
+        needle=name.strip().lower()
+        products=[p for p in products if needle in p.get("name","").lower()]
+
+        if not products:
+            raise HTTPException(status_code=404,detail=f"no product found matching name={name}")
+        
+    if sort_by == "price":
+        if order=="dsc":
+           products = sorted(products, key=lambda p: p["price"],reverse=True)
+        products = sorted(products, key=lambda p: p["price"])
+    total=len(products)
+
+    return {
+        "total":total,
+        "products":products
+    }
+
+@app.get("/products/{product_id}")
+def get_product_by_id(product_id:str=Path(
+    min_length=36,
+    max_length=36,
+    example="28282"
+)):
+    product=get_all_products()
+
+    for p in product:
+        if p["id"]==product_id:
+            return product
+        
+    raise HTTPException(status_code=404,detail="product not found")
